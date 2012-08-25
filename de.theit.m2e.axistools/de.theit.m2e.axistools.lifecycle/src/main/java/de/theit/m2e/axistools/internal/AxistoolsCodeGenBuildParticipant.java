130389198617dae8a41d38340f77cee092a4a972
kmullomukhametova <kmullomukhametova@cogniance.com>
2012-08-26 01:33:26 +0300

    skip rebuild if wsdl not changed or wsdl is url and rebuilded recently

--- de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenBuildParticipant.java
+++ de.theit.m2e.axistools/de.theit.m2e.axistools.lifecycle/src/main/java/de/theit/m2e/axistools/internal/AxistoolsCodeGenBuildParticipant.java
@@ -13,7 +13,9 @@ import java.io.File;
 import java.util.Set;
 
 import org.apache.maven.plugin.MojoExecution;
+import org.codehaus.plexus.util.Scanner;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.embedder.IMaven;
@@ -28,40 +30,79 @@ import org.sonatype.plexus.build.incremental.BuildContext;
  * @version $Id$
  */
 public class AxistoolsCodeGenBuildParticipant extends
-        MojoExecutionBuildParticipant {
-    /**
-     * Creates a new instance of this class.
-     * 
-     * @param execution
-     *            Mojo execution.
-     */
-    public AxistoolsCodeGenBuildParticipant(MojoExecution execution) {
-        super(execution, true);
-    }
+		MojoExecutionBuildParticipant {
+	private static final long URL_RELOAD_TIMEOUT = 24 * 60 * 60 * 1000;
+	
+	private long lastWsdlReloadTime;
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant#build(int,
-     *      org.eclipse.core.runtime.IProgressMonitor)
-     */
-    @Override
-    public Set<IProject> build(int kind, IProgressMonitor monitor)
-            throws Exception {
-        IMaven maven = MavenPlugin.getMaven();
-        BuildContext buildContext = getBuildContext();
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param execution
+	 *            Mojo execution.
+	 */
+	public AxistoolsCodeGenBuildParticipant(MojoExecution execution) {
+		super(execution, true);
+	}
 
-        // execute mojo
-        Set<IProject> result = super.build(kind, monitor);
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant#build(int,
+	 *      org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	public Set<IProject> build(int kind, IProgressMonitor monitor)
+			throws Exception {
+		IMaven maven = MavenPlugin.getMaven();
+		BuildContext buildContext = getBuildContext();
 
-        // tell m2e builder to refresh generated files
-        File outputDir = maven.getMojoParameterValue(getSession(),
-                getMojoExecution(), "outputDirectory", File.class);
-        File generated = AxistoolsResolveOutputUtil.getOutputFolder(outputDir, getMojoExecution());
-        if (null != generated) {
-            buildContext.refresh(generated);
-        }
+		if (skipRebuild(kind, maven, buildContext)) {
+			return null;
+		}
+		// execute mojo
+		Set<IProject> result = super.build(kind, monitor);
 
-        return result;
-    }
+		// tell m2e builder to refresh generated files
+		File outputDir = maven.getMojoParameterValue(getSession(),
+				getMojoExecution(), "outputDirectory", File.class);
+		File generated = AxistoolsResolveOutputUtil.getOutputFolder(outputDir,
+				getMojoExecution());
+		if (null != generated) {
+			buildContext.refresh(generated);
+		}
+
+		return result;
+	}
+
+	private boolean skipRebuild(int kind, IMaven maven,
+			BuildContext buildContext) throws CoreException {
+		String wsdlFileName = maven.getMojoParameterValue(getSession(),
+				getMojoExecution(), "wsdlFile", String.class);
+		
+		File wsdlFile = new File(wsdlFileName);
+		
+		// is url
+		if (!wsdlFile.exists() && wsdlFileName.startsWith("http")) {
+			long now = System.currentTimeMillis();
+			long delta = now - lastWsdlReloadTime;
+			if (delta > URL_RELOAD_TIMEOUT) {
+				lastWsdlReloadTime = now;
+				return false;
+			}
+			return true;
+		}
+		return !filesModified(buildContext, wsdlFile);
+	}
+
+	private boolean filesModified(BuildContext buildContext, File source) {
+		if (buildContext == null || source == null || !source.exists()) {
+			return false;
+		}
+
+		Scanner ds = buildContext.newScanner(source);
+		ds.scan();
+		String[] included = ds.getIncludedFiles();
+		return included != null && included.length > 0 ;
+	}
 }
